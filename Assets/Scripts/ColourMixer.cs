a8b907b977a4214f7018233c96cf680842f6d925
Daniel Jenkins <danieljenkins456@gmail.com>
2020-11-11 15:59:47 +1100

    Auto stash before merge of "DJ" and "origin/DanH"

---
+++ Assets/Scripts/ColourMixer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourMixer : MonoBehaviour
+{
+    public float r = 0.0f;
+    public float g = 0.0f;
+    public float b = 0.0f;
+    public Color color1;
+    public Color color2;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
