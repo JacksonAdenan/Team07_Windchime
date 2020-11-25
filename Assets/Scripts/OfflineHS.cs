38eb9c6a6db852f1b7c08c85a5ce64ff7d69ea47
Daniel Jenkins <danieljenkins456@gmail.com>
2020-11-25 17:33:27 +1100

    Highscore function (offline)
    added new test scene
    OfflineHS script

---
+++ Assets/Scripts/OfflineHS.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OfflineHS : MonoBehaviour
+{
+    public Transform entryContainer;
+    public Transform entryTemplate;
+    private List<Transform> highscoreEntryTransformList;
+    Highscores highscores;
+
+    private void Start()
+    {
+        //entryContainer = transform.Find("highscoreEntryContainer");
+        //entryTemplate = entryContainer.Find("highscoreEntryTemplate");
+
+        entryTemplate.gameObject.SetActive(false);
+
+        
+        AddHighscoreEntry(1000, "Jorb");
+        AddHighscoreEntry(900, "Jim");
+        AddHighscoreEntry(1020, "Fred");
+        AddHighscoreEntry(1300, "Margret");
+        AddHighscoreEntry(660, "Heath");
+        AddHighscoreEntry(2000, "Dean");
+        
+
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscores = JsonUtility.FromJson<Highscores>(jsonString);
+
+        // Sort entry list by score
+        for (int i = 0; i < highscores.highscoreEntryList.Count; i++)
+        {
+            for(int j = i + 1; j < highscores.highscoreEntryList.Count; j++)
+            {
+                if(highscores.highscoreEntryList[j].score > highscores.highscoreEntryList[i].score)
+                {
+                    //swap
+                    HighscoreEntry tmp = highscores.highscoreEntryList[i];
+                    highscores.highscoreEntryList[i] = highscores.highscoreEntryList[j];
+                    highscores.highscoreEntryList[j] = tmp;
+                }
+            }
+        }
+        highscoreEntryTransformList = new List<Transform>();
+        foreach(HighscoreEntry highscoreEntry in highscores.highscoreEntryList)
+        {
+            CreateHighscoreEntryTransform(highscoreEntry, entryContainer, highscoreEntryTransformList);
+        }
+
+    }
+
+    private void CreateHighscoreEntryTransform(HighscoreEntry highscoresEntry, Transform container, List<Transform> transformList)
+    {
+        float templateHeight = 30f;
+        Transform entryTransform = Instantiate(entryTemplate, container);
+        RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+        entryRectTransform.anchoredPosition = new Vector2(0, -templateHeight * transformList.Count);
+
+        int rank = transformList.Count + 1;
+        string rankString;
+        switch (rank)
+        {
+            default:
+                rankString = rank + "TH"; break;
+
+            case 1: rankString = "1ST"; break;
+            case 2: rankString = "2ND"; break;
+            case 3: rankString = "3RD"; break;
+        }
+        entryTransform.GetChild(0).GetComponent<Text>().text = rankString;
+
+        int score = highscoresEntry.score;
+        entryTransform.GetChild(1).GetComponent<Text>().text = score.ToString();
+
+        string name = highscoresEntry.name;
+        entryTransform.GetChild(2).GetComponent<Text>().text = name;
+        transformList.Add(entryTransform);
+    }
+
+    private void AddHighscoreEntry(int score, string name)
+    {
+        //create highscoreEntry
+        HighscoreEntry highscoreEntry = new HighscoreEntry { score = score, name = name };
+
+        //Load saved Highscores
+        string jsonString = PlayerPrefs.GetString("highscoreTable");
+        Highscores highscore = JsonUtility.FromJson<Highscores>(jsonString);
+
+        //Add new entry to Highscores
+        highscore.highscoreEntryList.Add(highscoreEntry);
+
+        //save updated Highscores
+        //string json = JsonUtility.ToJson(highscore);
+        //PlayerPrefs.SetString("highscoreTable", json);
+        //PlayerPrefs.Save();
+    }
+
+    private class Highscores
+    {
+        public List<HighscoreEntry> highscoreEntryList;
+    }
+
+
+    [System.Serializable]
+    private class HighscoreEntry
+    {
+        public int score;
+        public string name;
+    }
+
+
+}
